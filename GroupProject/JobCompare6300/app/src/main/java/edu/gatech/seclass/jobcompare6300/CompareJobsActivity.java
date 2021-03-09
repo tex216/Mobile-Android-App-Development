8a39021c3769c3faf00eae54a954ea1352e9266d
Zhengning Han <zhan92@gatech.edu>
2021-03-08 23:20:05 -0500

    resolved some build issues

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CompareJobsActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CompareJobsActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CompareJobActivity extends AppCompatActivity {
+public class CompareJobsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compare_job);
+        setContentView(R.layout.activity_compare_jobs);
     }
-
 }
--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/RankedListActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/RankedListActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RankedList extends AppCompatActivity {
+public class RankedListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
