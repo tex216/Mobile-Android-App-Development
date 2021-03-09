119bbae0f0547d757a3db59e36d4770389cc0b91
Zhengning Han <zhan92@gatech.edu>
2021-03-08 22:22:00 -0500

    added comparison settings activity

---
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/AdjustComparisonSettingsActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdjustComparisonSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adjust_comparison_settings);
+    }
+}
