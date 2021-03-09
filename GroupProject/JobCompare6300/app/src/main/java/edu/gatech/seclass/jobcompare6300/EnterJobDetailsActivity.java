016243a7de656fad18d6a98a67ef7263a0727d1f
Zhengning Han <zhan92@gatech.edu>
2021-03-08 22:04:06 -0500

    added enter job details layout + navigation from main menu

---
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterJobDetailsActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterJobDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_job_details);
+    }
+}
--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
@@ -2,13 +2,37 @@ package edu.gatech.seclass.jobcompare6300;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enterCurrentJob;
+    private Button enterJobOffers;
+    private Button adjustComparisonSettings;
+    private Button compareJobs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterCurrentJob = (Button) findViewById(R.id.btn_enter_current_job);
+        enterJobOffers = (Button) findViewById(R.id.btn_enter_job_offers);
+        adjustComparisonSettings = (Button) findViewById(R.id.btn_adjust_comp_settings);
+        compareJobs = (Button) findViewById(R.id.btn_compare_job_offers);
+
+        enterCurrentJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleEnterCurrentJobClick();
+            }
+        });
+    }
+
+    public void handleEnterCurrentJobClick() {
+        Intent intent = new Intent(this, EnterJobDetailsActivity.class);
+        startActivity(intent);
     }
 }
