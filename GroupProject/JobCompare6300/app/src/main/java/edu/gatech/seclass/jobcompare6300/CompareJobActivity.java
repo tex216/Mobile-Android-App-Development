baff0313f15702b5b32a5c10ef51734d58293332
Edmond Truong <etruong7@gatech.edu>
2021-03-08 21:58:38 -0600

    Update CompareJobActivity.java

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CompareJobActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CompareJobActivity.java
@@ -1,21 +1,15 @@
 package edu.gatech.seclass.jobcompare6300;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
-public class CompareJobActivity extends WearableActivity {
-
-    private TextView mTextView;
+public class CompareJobActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_job);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
+
 }
