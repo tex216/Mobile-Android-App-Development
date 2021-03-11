52023a09b4f1408c5f3ca6ea841296f3ce50a338
Zhengning Han <zhan92@gatech.edu>
2021-03-10 21:20:55 -0500

    added enter job offers activity

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterCurrentJobActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterCurrentJobActivity.java
@@ -1,12 +1,14 @@
 package edu.gatech.seclass.jobcompare6300;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class EnterCurrentJobActivity extends AppCompatActivity {
     private Button save;
@@ -15,8 +17,8 @@ public class EnterCurrentJobActivity extends AppCompatActivity {
     private EditText company;
     private EditText city;
     private EditText state;
-    private EditText costofLiveing;
-    private EditText remoteWork;
+    private EditText costofLiving;
+    private Spinner remoteWork;
     private EditText salary;
     private EditText yearlyBonus;
     private EditText retirement;
@@ -29,19 +31,20 @@ public class EnterCurrentJobActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_current_job);
-        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-jobcompare").allowMainThreadQueries().build();
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-jobcompare2").allowMainThreadQueries().build();
         save = (Button) findViewById(R.id.btn_save_cj);
         cancel = (Button) findViewById(R.id.btn_cancel_cj);
         title = (EditText)findViewById(R.id.text_title);
         company = (EditText)findViewById(R.id.text_company);;
         city = (EditText)findViewById(R.id.text_city);;
         state = (EditText)findViewById(R.id.text_state);;
-        costofLiveing = (EditText)findViewById(R.id.number_cost_of_living);;
-//        remoteWork = (EditText)findViewById(R.id.dropdown_remote_work);;
+        costofLiving = (EditText)findViewById(R.id.number_cost_of_living);;
+        remoteWork = (Spinner) findViewById(R.id.dropdown_remote_work);;
         salary = (EditText)findViewById(R.id.number_salary);;
         yearlyBonus = (EditText)findViewById(R.id.number_bonus);;
         retirement = (EditText)findViewById(R.id.number_retirement);;
         leaveTime = (EditText)findViewById(R.id.number_leave_time);;
+
 
 
         save.setOnClickListener(new View.OnClickListener() {
@@ -69,11 +72,11 @@ public class EnterCurrentJobActivity extends AppCompatActivity {
         jobDetails.STATE = state.getText().toString();
         jobDetails.YEARLY_SALARY = Double.parseDouble(salary.getText().toString());
         jobDetails.YEARLY_BONUS = Double.parseDouble(yearlyBonus.getText().toString());
-        jobDetails.COST_OF_LIVING_INDEX = 5;
-        jobDetails.IS_CURRENT_JOB = false;
+        jobDetails.COST_OF_LIVING_INDEX = Integer.parseInt(costofLiving.getText().toString());
+        jobDetails.IS_CURRENT_JOB = true;
         jobDetails.LEAVE_TIME = Integer.parseInt(leaveTime.getText().toString());
         jobDetails.PERCENTAGE_MATCHED = Double.parseDouble(retirement.getText().toString());
-        jobDetails.WORK_REMOTE = Integer.parseInt(remoteWork.getText().toString());
+        jobDetails.WORK_REMOTE = Integer.parseInt(remoteWork.getSelectedItem().toString());
         jobDetails.SCORE = null;
         jobDetailsDao.insertJob(jobDetails);
 
