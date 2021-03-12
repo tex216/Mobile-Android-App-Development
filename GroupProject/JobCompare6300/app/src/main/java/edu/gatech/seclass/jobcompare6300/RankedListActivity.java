8bbe5e97eb39a50ebace4b214bd49e311baf3540
Yi Wang <ywang3701@gatech.edu>
2021-03-11 20:32:15 -0500

    add job list

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/RankedListActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/RankedListActivity.java
@@ -8,8 +8,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,6 +24,9 @@ public class RankedListActivity extends AppCompatActivity {
     private Button cancel;
     private final Context context = this;
     private AppDatabase appDatabase;
+    private ArrayList<String> listItem;
+    private ArrayAdapter adapter;
+    ListView ranked_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,14 @@ public class RankedListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ranked_list);
 
         appDatabase = AppDatabase.getInstance(context);
+        ranked_list = findViewById(R.id.ranked_list);
 
         compare = (Button) findViewById(R.id.btn_compare_ranked_list);
         cancel = (Button) findViewById(R.id.btn_return_ranked_list);
+
+        listItem = new ArrayList<>();
+
+        viewData();
 
         compare.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,13 +57,40 @@ public class RankedListActivity extends AppCompatActivity {
             }
         });
 
+        JobDetailsDao jobDetailsDao = this.appDatabase.jobDetailsDao();
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            List<JOB_DETAILS> allJobs = jobDetailsDao.getAllJobs();
+            for (int i = 0; i<allJobs.size();i++){
 
-        JobDetailsDao jobDetailsDao = this.appDatabase.jobDetailsDao();
+            }
 
-        List<JOB_DETAILS> allJobs = jobDetailsDao.getAllJobs();
+        });
     }
+    public void viewData(){
+        JobDetailsDao jobDetailsDao = this.appDatabase.jobDetailsDao();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            List<JOB_DETAILS> allJobs = jobDetailsDao.getAllJobs();
+
+            for (int i = 0; i<allJobs.size();i++){
+                JOB_DETAILS job = allJobs.get(i);
+                if(job.getIS_CURRENT_JOB()==true){
+                    listItem.add("Title: " + job.getTITLE() + " (Current Job)"+"\n" + "Company: " + job.getCOMPANY());
+                }else{
+                    listItem.add("Title: " + job.getTITLE() + "\n" + "Company: " + job.getCOMPANY());
+                }
+            }
+
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listItem);
+            ranked_list.setAdapter(adapter);
+
+
+        });
+    }
+
 
     public void handleCompareJobsClick() {
         Intent intent = new Intent(this, CompareJobsActivity.class);
